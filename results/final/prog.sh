05f765f6b8867ff80143c4f5386250294a91752f
sam_s <sadki.sami95@gmail.com>
2020-01-17 16:29:29 +0100

    crash

--- results/final/prog.sh
+++ results/final/prog.sh
@@ -1,3 +1,3 @@
 la grosse moula, du carameeeeel
 
-moi dabordlnjefnsdkvnjbrgj;bsdjfvbjdb;jvnqlrkjgblqjkdfbkjqdfg;jbqdfbvfi
+moi dabordlnjefnsdkvnjbrgj;bsdjfvbjdb;jvnqlrkjgblqjkdfbkjqdfg;jbqdfbvfdfvnc,n ,;dnf nsdkf;nv;dnsf,v;nd;v,nd;fnnfndf;,v;df;vdfi
